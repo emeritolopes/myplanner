bcf4f45237d293e493da13db6d09c31cc2c5bf72
emeritolopes <emerito.lopes@gmail.com>
2024-01-07 14:56:10 +0000

    add date and tiem

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-    console.log("Ready!");
-});
-    //Display current date and time (Moment.js)
-    let now = moment().format("dddd, MMMM Do YYYY");
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = now;
-    let currentHour = moment().format("HH");
+// $(document).ready(function() {
+//     console.log("Ready!");
+// });
+//     //Display current date and time (Moment.js)
+//     let now = moment().format("dddd, MMMM Do YYYY");
+//     let displayDate = document.getElementById("currentDay");
+//     displayDate.innerHTML = now;
+//     let currentHour = moment().format("HH");
 
+$(document).ready(function () {
 
-   
+//display current date and time
+    var displayTime = document.querySelector("#currentDay");
