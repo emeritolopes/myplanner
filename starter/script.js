913929efedbcc1ec258d8e566f3867702ad99df3
emeritolopes <emerito.lopes@gmail.com>
2024-01-07 15:11:56 +0000

    added event listener and localstorage

--- starter/script.js
+++ starter/script.js
@@ -14,7 +14,13 @@ $(document).ready(function () {
     displayTime.textContent = currentTime;
 //display time and date using dayjs
     var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
-
     displayTime.textContent = currentTime;
 
-});
+// adding button to save input in local storage when click event listener
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+    
+        
+        localStorage.setItem(time, text);
+    });
